b72fa7de760cfa2ed95b059be9989abbe5e703fa
Zhen Chen <zhenchen@Zhens-iMac.local>
2020-10-17 18:39:57 -0700

    appx init

--- service/schema/r001.p01/appx.sql
+++ service/schema/r001.p01/appx.sql
@@ -1,4 +1,8 @@
 DROP TABLE IF EXISTS `appx`.`_appx_meta`;
+DROP TABLE IF EXISTS `appx`.`_role_grant`;
+DROP TABLE IF EXISTS `appx`.`_role_scope`;
+DROP TABLE IF EXISTS `appx`.`_perm_func`;
+DROP TABLE IF EXISTS `appx`.`_perm_obj`;
 DROP TABLE IF EXISTS `appx`.`namespace`;
 DROP TABLE IF EXISTS `appx`.`namespace_status`;
 DROP TABLE IF EXISTS `appx`.`env`;
@@ -25,6 +29,60 @@ CREATE TABLE IF NOT EXISTS `appx`.`_appx_meta` (
     `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `deleted`               TINYINT(1)              NOT NULL DEFAULT 0,
     UNIQUE INDEX idx_metadata(appx_name, appx_key),
+    PRIMARY KEY (`id`)
+)
+CHARACTER SET utf8 COLLATE utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `appx`.`_role_grant` (
+    `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
+    `role_name`             VARCHAR(32)             NOT NULL,
+    `grant_scope`           VARCHAR(32)             NOT NULL,
+    `grant_name`            VARCHAR(32)             NOT NULL,
+    `grant_spec`            JSON                    NOT NULL,
+    `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted`               TINYINT(1)              NOT NULL DEFAULT 0,
+    UNIQUE INDEX idx_app(role_name, grant_scope, grant_name),
+    PRIMARY KEY (`id`)
+)
+CHARACTER SET utf8 COLLATE utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `appx`.`_role_scope` (
+    `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
+    `scope_name`            VARCHAR(32)             NOT NULL,
+    `scope_spec`            JSON                    NOT NULL,
+    `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted`               TINYINT(1)              NOT NULL DEFAULT 0,
+    UNIQUE INDEX idx_app(scope_name),
+    PRIMARY KEY (`id`)
+)
+CHARACTER SET utf8 COLLATE utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `appx`.`_perm_func` (
+    `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
+    `role_name`             VARCHAR(32)             NOT NULL,
+    `func_scope`            VARCHAR(32)             NOT NULL,
+    `func_name`             VARCHAR(32)             NOT NULL,
+    `func_spec`             JSON                    NOT NULL,
+    `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted`               TINYINT(1)              NOT NULL DEFAULT 0,
+    UNIQUE INDEX idx_app(role_name, func_scope, func_name),
+    PRIMARY KEY (`id`)
+)
+CHARACTER SET utf8 COLLATE utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `appx`.`_perm_obj` (
+    `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
+    `role_name`             VARCHAR(32)             NOT NULL,
+    `obj_scope`             VARCHAR(32)             NOT NULL,
+    `obj_key`               VARCHAR(32)             NOT NULL,
+    `obj_spec`              JSON                    NOT NULL,
+    `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted`               TINYINT(1)              NOT NULL DEFAULT 0,
+    UNIQUE INDEX idx_app(role_name, obj_scope, obj_key),
     PRIMARY KEY (`id`)
 )
 CHARACTER SET utf8 COLLATE utf8_bin;
@@ -288,4 +346,8 @@ CREATE TABLE IF NOT EXISTS `appx`.`transform_status` (
 CHARACTER SET utf8 COLLATE utf8_bin
 PARTITION BY KEY(namespace, app_name) PARTITIONS 10;
 
-INSERT INTO `appx`.`_appx_meta`(`appx_name`, `appx_key`, `appx_info`) VALUES ('version', 'r001.p01', '{"comment": "initial version"}');
+INSERT INTO `appx`.`_role_grant`(`role_name`, `grant_scope`, `grant_name`, `grant_spec`) VALUES ('appx.superadmin', 'appx.local_auth', 'appx', '{"comment": "initial grant"}');
+INSERT INTO `appx`.`_role_scope`(`scope_name`, `scope_spec`) VALUES ('appx.local_auth', '{"comment": "initial scope"}');
+
+INSERT INTO `appx`.`_appx_meta`(`appx_name`, `appx_key`, `appx_info`) VALUES ('appx.version', 'r001.p01', '{"comment": "initial version"}') ON DUPLICATE KEY UPDATE appx_info=VALUES(appx_info);
+INSERT INTO `appx`.`_appx_meta`(`appx_name`, `appx_key`, `appx_info`) VALUES ('appx.local_auth', 'appx', JSON_OBJECT('password', PASSWORD("{{appx.init.admin_password}}"), 'comment', 'initial version')) ON DUPLICATE KEY UPDATE appx_info=VALUES(appx_info);
