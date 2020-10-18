86314ba6878db0e7949eb0241f7ac3ccf8f11549
Draco Tadpole <gsk390@gmail.com>
2020-10-18 01:16:17 -0700

    fix

--- service/init.d/Linux/init.template.sh
+++ service/init.d/Linux/init.template.sh
@@ -8,6 +8,10 @@ sudo groupadd -r -g {{appx.init.service_gid_appx}} {{appx.init.service_grp_appx}
 sudo userdel {{appx.init.service_usr_appx}}
 sudo useradd -r -s /usr/sbin/nologin -u {{appx.init.service_uid_appx}} -g {{appx.init.service_gid_appx}} {{appx.init.service_usr_appx}}
 
+
+rm -fR /tmp/$$
+mkdir -p /tmp/$$
+
 # install appx_mysql
 echo "=========="
 echo ${NODE} ${BASE_DIR}/common.d/eval_template.js --template ${BASE_DIR}/init.d/`uname`/appx_mysql.template.service --yaml /tmp/$$/init.yaml
@@ -31,3 +35,6 @@ sudo systemctl daemon-reload
 sudo systemctl enable appx_node
 sudo systemctl restart appx_node
 sudo systemctl status appx_node
+
+echo "----------"
+rm -fR /tmp/$$
