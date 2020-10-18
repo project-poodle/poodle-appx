7f93f0266af3ff63384780c9bbccac74c9db5b43
Draco Tadpole <gsk390@gmail.com>
2020-10-17 19:24:09 -0700

    refactor

--- service/schema/r001.p01/appx.sql
+++ service/schema/r001.p01/appx.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS `appx`.`_appx_meta`;
-DROP TABLE IF EXISTS `appx`.`_role_grant`;
 DROP TABLE IF EXISTS `appx`.`_role_scope`;
+DROP TABLE IF EXISTS `appx`.`_role_grant`;
 DROP TABLE IF EXISTS `appx`.`_perm_func`;
 DROP TABLE IF EXISTS `appx`.`_perm_obj`;
 DROP TABLE IF EXISTS `appx`.`namespace`;
@@ -9,6 +9,8 @@ DROP TABLE IF EXISTS `appx`.`env`;
 DROP TABLE IF EXISTS `appx`.`env_status`;
 DROP TABLE IF EXISTS `appx`.`app`;
 DROP TABLE IF EXISTS `appx`.`app_status`;
+DROP TABLE IF EXISTS `appx`.`deployment`;
+DROP TABLE IF EXISTS `appx`.`deployment_status`;
 DROP TABLE IF EXISTS `appx`.`obj`;
 DROP TABLE IF EXISTS `appx`.`obj_status`;
 DROP TABLE IF EXISTS `appx`.`attr`;
@@ -33,6 +35,18 @@ CREATE TABLE IF NOT EXISTS `appx`.`_appx_meta` (
 )
 CHARACTER SET utf8 COLLATE utf8_bin;
 
+CREATE TABLE IF NOT EXISTS `appx`.`_role_scope` (
+    `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
+    `scope_name`            VARCHAR(32)             NOT NULL,
+    `scope_spec`            JSON                    NOT NULL,
+    `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted`               TINYINT(1)              NOT NULL DEFAULT 0,
+    UNIQUE INDEX idx_app(scope_name),
+    PRIMARY KEY (`id`)
+)
+CHARACTER SET utf8 COLLATE utf8_bin;
+
 CREATE TABLE IF NOT EXISTS `appx`.`_role_grant` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `role_name`             VARCHAR(32)             NOT NULL,
@@ -43,18 +57,6 @@ CREATE TABLE IF NOT EXISTS `appx`.`_role_grant` (
     `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `deleted`               TINYINT(1)              NOT NULL DEFAULT 0,
     UNIQUE INDEX idx_app(role_name, grant_scope, grant_name),
-    PRIMARY KEY (`id`)
-)
-CHARACTER SET utf8 COLLATE utf8_bin;
-
-CREATE TABLE IF NOT EXISTS `appx`.`_role_scope` (
-    `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
-    `scope_name`            VARCHAR(32)             NOT NULL,
-    `scope_spec`            JSON                    NOT NULL,
-    `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `deleted`               TINYINT(1)              NOT NULL DEFAULT 0,
-    UNIQUE INDEX idx_app(scope_name),
     PRIMARY KEY (`id`)
 )
 CHARACTER SET utf8 COLLATE utf8_bin;
@@ -114,7 +116,7 @@ CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE TABLE IF NOT EXISTS `appx`.`env` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `env_name`              VARCHAR(32)             NOT NULL,
+    `env_name`              VARCHAR(15)             NOT NULL,
     `env_spec`              JSON                    NOT NULL,
     `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -127,7 +129,7 @@ CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE TABLE IF NOT EXISTS `appx`.`env_status` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `env_name`              VARCHAR(32)             NOT NULL,
+    `env_name`              VARCHAR(15)             NOT NULL,
     `env_state`             JSON                    NOT NULL,
     `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -140,7 +142,7 @@ CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE TABLE IF NOT EXISTS `appx`.`app` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `app_spec`              JSON                    NOT NULL,
     `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -154,14 +156,43 @@ CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE TABLE IF NOT EXISTS `appx`.`app_status` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `env_name`              VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `app_state`             JSON                    NOT NULL,
     `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `deleted`               TINYINT(1)              NOT NULL DEFAULT 0,
-    UNIQUE INDEX idx_app(namespace, env_name, app_name, app_ver),
+    UNIQUE INDEX idx_app(namespace, app_name, app_ver),
+    PRIMARY KEY (`id`)
+)
+CHARACTER SET utf8 COLLATE utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `appx`.`deployment` (
+    `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
+    `namespace`             VARCHAR(32)             NOT NULL,
+    `env_name`              VARCHAR(15)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
+    `app_ver`               VARCHAR(32)             NOT NULL,
+    `deployment_spec`       JSON                    NOT NULL,
+    `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted`               TINYINT(1)              NOT NULL DEFAULT 0,
+    UNIQUE INDEX idx_app(namespace, env_name, app_name),
+    PRIMARY KEY (`id`)
+)
+CHARACTER SET utf8 COLLATE utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `appx`.`deployment_status` (
+    `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
+    `namespace`             VARCHAR(32)             NOT NULL,
+    `env_name`              VARCHAR(15)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
+    `app_ver`               VARCHAR(32)             NOT NULL,
+    `deployment_status`     JSON                    NOT NULL,
+    `create_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time`           TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted`               TINYINT(1)              NOT NULL DEFAULT 0,
+    UNIQUE INDEX idx_app(namespace, env_name, app_name),
     PRIMARY KEY (`id`)
 )
 CHARACTER SET utf8 COLLATE utf8_bin;
@@ -169,7 +200,7 @@ CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE TABLE IF NOT EXISTS `appx`.`obj` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `obj_name`              VARCHAR(32)             NOT NULL,
     `obj_spec`              JSON                    NOT NULL,
@@ -184,8 +215,8 @@ CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE TABLE IF NOT EXISTS `appx`.`obj_status` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `env_name`              VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `env_name`              VARCHAR(15)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `obj_name`              VARCHAR(32)             NOT NULL,
     `obj_state`             JSON                    NOT NULL,
@@ -200,7 +231,7 @@ CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE TABLE IF NOT EXISTS `appx`.`relation` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `obj_name`              VARCHAR(32)             NOT NULL,
     `objn_name`             VARCHAR(32)             NOT NULL,
@@ -216,8 +247,8 @@ CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE TABLE IF NOT EXISTS `appx`.`relation_status` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `env_name`              VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `env_name`              VARCHAR(15)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `obj_name`              VARCHAR(32)             NOT NULL,
     `objn_name`             VARCHAR(32)             NOT NULL,
@@ -233,7 +264,7 @@ CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE TABLE IF NOT EXISTS `appx`.`attr` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `obj_name`              VARCHAR(32)             NOT NULL,
     `attr_name`             VARCHAR(32)             NOT NULL,
@@ -250,8 +281,8 @@ PARTITION BY KEY(namespace, app_name) PARTITIONS 10;
 CREATE TABLE IF NOT EXISTS `appx`.`attr_status` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `env_name`              VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `env_name`              VARCHAR(15)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `obj_name`              VARCHAR(32)             NOT NULL,
     `attr_name`             VARCHAR(32)             NOT NULL,
@@ -268,7 +299,7 @@ PARTITION BY KEY(namespace, app_name) PARTITIONS 10;
 CREATE TABLE IF NOT EXISTS `appx`.`api` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `obj_name`              VARCHAR(32)             NOT NULL,
     `api_method`            VARCHAR(16)             NOT NULL,
@@ -286,8 +317,8 @@ PARTITION BY KEY(namespace, app_name) PARTITIONS 10;
 CREATE TABLE IF NOT EXISTS `appx`.`api_status` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `env_name`              VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `env_name`              VARCHAR(15)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `obj_name`              VARCHAR(32)             NOT NULL,
     `api_method`            VARCHAR(16)             NOT NULL,
@@ -305,7 +336,7 @@ PARTITION BY KEY(namespace, app_name) PARTITIONS 10;
 CREATE TABLE IF NOT EXISTS `appx`.`transform` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `obj_name`              VARCHAR(32)             NOT NULL,
     `src_namespace`         VARCHAR(32)             NOT NULL,
@@ -326,8 +357,8 @@ PARTITION BY KEY(namespace, app_name) PARTITIONS 10;
 CREATE TABLE IF NOT EXISTS `appx`.`transform_status` (
     `id`                    BIGINT                  NOT NULL AUTO_INCREMENT,
     `namespace`             VARCHAR(32)             NOT NULL,
-    `env_name`              VARCHAR(32)             NOT NULL,
-    `app_name`              VARCHAR(32)             NOT NULL,
+    `env_name`              VARCHAR(15)             NOT NULL,
+    `app_name`              VARCHAR(15)             NOT NULL,
     `app_ver`               VARCHAR(32)             NOT NULL,
     `obj_name`              VARCHAR(32)             NOT NULL,
     `src_namespace`         VARCHAR(32)             NOT NULL,
@@ -346,8 +377,10 @@ CREATE TABLE IF NOT EXISTS `appx`.`transform_status` (
 CHARACTER SET utf8 COLLATE utf8_bin
 PARTITION BY KEY(namespace, app_name) PARTITIONS 10;
 
-INSERT INTO `appx`.`_role_grant`(`role_name`, `grant_scope`, `grant_name`, `grant_spec`) VALUES ('appx.superadmin', 'appx.local_auth', 'appx', '{"comment": "initial grant"}');
-INSERT INTO `appx`.`_role_scope`(`scope_name`, `scope_spec`) VALUES ('appx.local_auth', '{"comment": "initial scope"}');
+INSERT INTO `appx`.`_appx_meta`(`appx_name`, `appx_key`, `appx_info`) VALUES ('appx.version',       'r001.p01',                     JSON_OBJECT('comment', 'initialize')) ON DUPLICATE KEY UPDATE appx_info=VALUES(appx_info);
+INSERT INTO `appx`.`_appx_meta`(`appx_name`, `appx_key`, `appx_info`) VALUES ('appx.schema',        'prefix',                       JSON_OBJECT('value', '{{appx.init.schema_prefix}}', 'comment', 'initialize')) ON DUPLICATE KEY UPDATE appx_info=VALUES(appx_info);
+INSERT INTO `appx`.`_appx_meta`(`appx_name`, `appx_key`, `appx_info`) VALUES ('appx.schema',        'separator',                    JSON_OBJECT('value', '{{appx.init.schema_separator}}', 'comment', 'initialize')) ON DUPLICATE KEY UPDATE appx_info=VALUES(appx_info);
+INSERT INTO `appx`.`_appx_meta`(`appx_name`, `appx_key`, `appx_info`) VALUES ('appx.local_auth',    '{{appx.init.admin_user}}',     JSON_OBJECT('password', PASSWORD("{{appx.init.admin_pass}}"), 'comment', 'initialize')) ON DUPLICATE KEY UPDATE appx_info=VALUES(appx_info);
 
-INSERT INTO `appx`.`_appx_meta`(`appx_name`, `appx_key`, `appx_info`) VALUES ('appx.version', 'r001.p01', '{"comment": "initial version"}') ON DUPLICATE KEY UPDATE appx_info=VALUES(appx_info);
-INSERT INTO `appx`.`_appx_meta`(`appx_name`, `appx_key`, `appx_info`) VALUES ('appx.local_auth', 'appx', JSON_OBJECT('password', PASSWORD("{{appx.init.admin_password}}"), 'comment', 'initial version')) ON DUPLICATE KEY UPDATE appx_info=VALUES(appx_info);
+INSERT INTO `appx`.`_role_scope`(`scope_name`, `scope_spec`) VALUES ('appx.local_auth', '{"comment": "initial scope"}') ON DUPLICATE KEY UPDATE scope_spec=VALUES(scope_spec);
+INSERT INTO `appx`.`_role_grant`(`role_name`, `grant_scope`, `grant_name`, `grant_spec`) VALUES ('appx.superadmin', 'appx.local_auth', 'appx', '{"comment": "initial grant"}') ON DUPLICATE KEY UPDATE grant_spec=VALUES(grant_spec);
