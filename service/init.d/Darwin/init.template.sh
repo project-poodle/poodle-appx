e1edc3d450cd24eeaf61b375cc65e1238e624015
Draco Tadpole <gsk390@gmail.com>
2020-10-18 01:18:26 -0700

    fix

--- service/init.d/Darwin/init.template.sh
+++ service/init.d/Darwin/init.template.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-# create group appx
+# cleanup user and group
+sudo dscl . -delete /Users/{{appx.init.service_usr_appx}}
 sudo dscl . -delete /Groups/{{appx.init.service_grp_appx}}
+
+# create group appx
 sudo dscl . -create /Groups/{{appx.init.service_grp_appx}}
 sudo dscl . -create /Groups/{{appx.init.service_grp_appx}} gid {{appx.init.service_gid_appx}}
 
 # create user appx
-sudo dscl . -delete /Users/{{appx.init.service_usr_appx}}
 sudo dscl . -create /Users/{{appx.init.service_usr_appx}}
 sudo dscl . -create /Users/{{appx.init.service_usr_appx}} UniqueID {{appx.init.service_uid_appx}}
 sudo dscl . -create /Users/{{appx.init.service_usr_appx}} PrimaryGroupID {{appx.init.service_gid_appx}}
--- service/init.d/Linux/init.template.sh
+++ service/init.d/Linux/init.template.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-# create group appx
+# cleanup user and group
+sudo userdel {{appx.init.service_usr_appx}}
 sudo groupdel {{appx.init.service_grp_appx}}
+
+# create group appx
 sudo groupadd -r -g {{appx.init.service_gid_appx}} {{appx.init.service_grp_appx}}
 
 # create user appx
-sudo userdel {{appx.init.service_usr_appx}}
 sudo useradd -r -s /usr/sbin/nologin -u {{appx.init.service_uid_appx}} -g {{appx.init.service_gid_appx}} {{appx.init.service_usr_appx}}
 
 
