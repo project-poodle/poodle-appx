0bd09e80900a41c68a1845552285a255eb01e1a4
Draco Tadpole <gsk390@gmail.com>
2020-10-20 01:07:42 -0700

    update template

--- service/schema.d/r001.p01/run.sh
+++ service/schema.d/r001.p01/run.sh
@@ -14,7 +14,7 @@ source ${BASE_DIR}/common.d/func.sh
 rm -fR /tmp/$$
 mkdir -p /tmp/$$
 
-eval_template -t appx.yaml -y1 appx.yaml -ys1 SELF -j1 ${BASE_DIR}/conf.d/mysql_appx.json -js1 MYSQL_CONF > /tmp/$$/appx.yaml
+eval_template -t appx.yaml -y1 appx.yaml -j1 ${BASE_DIR}/conf.d/mysql_appx.json -js1 MYSQL_CONF > /tmp/$$/appx.yaml
 cat /tmp/$$/appx.yaml
 
 eval_template -t appx.sql -y1 /tmp/$$/appx.yaml -y2 obj.yaml -y3 relation.yaml -y4 attr.yaml -y5 api.yaml
