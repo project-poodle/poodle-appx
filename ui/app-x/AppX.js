3ffd5c60ab6bc8cd16bd8ec6348c410f510043cd
Draco Tadpole <37931043+DracoTadpole@users.noreply.github.com>
2020-11-29 04:38:54 -0800

    router

--- ui/app-x/AppX.js
+++ ui/app-x/AppX.js
@@ -1,9 +1,8 @@
-       //import 'react-perfect-scrollbar/dist/css/styles.css';
+//import 'react-perfect-scrollbar/dist/css/styles.css';
 //const React = lib['react']
 import React from 'react'
 import { useRoutes } from 'hookrouter'
 import PropTypes from 'prop-types'
-//const MaterialUI = lib['@material-ui/core']
 import { ThemeProvider, Box, Button, Grid, CssBaseline, makeStyles } from '@material-ui/core'
 import { Provider } from 'react-redux'
 
@@ -11,10 +10,9 @@ import theme from 'app-x/theme'
 import GlobalStyles from 'app-x/components/GlobalStyles'
 import HeaderLayout from 'app-x/pages/layouts/headerLayout'
 import NotFoundView from 'app-x/views/errors/NotFoundView'
+
 import store from 'app-x/redux/store'
-
 import routes from 'app-x/routes.js'
-
 
 const App = (props, children) => {
 
---
+++ ui/app-x/router.js
@@ -0,0 +1,7 @@
+import { useRoutes, navigate, A } from 'hookrouter'
+
+export {
+  A,
+  navigate,
+  useRoutes,
+}
