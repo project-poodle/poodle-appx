d0e35bddf32c0b8b03f5e6cbbb9c80d3241a06bf
Draco Tadpole <37931043+DracoTadpole@users.noreply.github.com>
2020-12-01 05:36:45 -0800

    tweak

--- ui/app-x/theme/index.js
+++ ui/app-x/theme/index.js
@@ -1,7 +1,7 @@
 //import MaterialUI from '@material-ui/core';
 //const MaterialUI = lib['@material-ui/core']
 
-import typography from 'app-x/theme/topography.js'
+import typography from 'app-x/theme/topography'
 
 //const { createMuiTheme, colors } = MaterialUI;
 import { createMuiTheme, colors } from '@material-ui/core'
