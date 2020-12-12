e3d1576564254879879fe98ba2a7240aaf81d7cb
Draco Tadpole <37931043+DracoTadpole@users.noreply.github.com>
2020-12-12 11:37:39 -0800

    token support

--- app/src/auth/index.js
+++ app/src/auth/index.js
@@ -1,18 +1,13 @@
 /**
  * Module dependencies.
  */
-// const { authenticator, loginUserWithPass, logoutUser, findUserWithPass } = require('./authenticator')
 const { authenticator } = require('./authenticator')
 const { auth_dispatcher } = require('./auth_router')
-// const {  } = require('./auth_local')
 
 /**
- * Export constructors.
+ * Export
  */
  module.exports = {
      authenticator: authenticator,
      auth_dispatcher: auth_dispatcher,
-     // findUserWithPass: findUserWithPass,
-     // loginUserWithPass: loginUserWithPass,
-     // logoutUser: logoutUser,
  }
