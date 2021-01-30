b72584a806ec87d88ae890c1ac12f67a45e1f19c
Draco Tadpole <37931043+DracoTadpole@users.noreply.github.com>
2021-01-30 02:36:36 -0800

    tweak

--- ui/app-x/theme/GlobalStyleProvider.js
+++ ui/app-x/theme/GlobalStyleProvider.js
@@ -90,8 +90,8 @@ const GlobalStyleProvider = (props) => {
       },
       '#root': {
         backgroundColor: theme?.palette?.background?.paper,
-        // height: '100%',
-        // width: '100%'
+        height: '100%',
+        width: '100%'
       },
       // antd overrides
       'h1': {
