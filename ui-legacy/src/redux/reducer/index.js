52d596b7dfee231342b422e5e78655d5feb87282
Draco Tadpole <37931043+DracoTadpole@users.noreply.github.com>
2021-02-04 01:22:26 -0800

    remove ui-legacy

--- ui-legacy/src/redux/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import userReducer from './userReducer'
-import msgReducer from './msgReducer'
-
-export default combineReducers({
-  userReducer,
-  msgReducer,
-})
--- ui-legacy/src/test/acorn.js
+++
@@ -1,9 +0,0 @@
-const acorn = require("acorn");
-const jsx = require("acorn-jsx");
-const escodegen = require("escodegen");
-
-const parsed = acorn.Parser.extend(jsx()).parse("my(<jsx/>, 'code');");
-console.log(parsed)
-
-const program = escodegen.generate(parsed)
-console.log(program)
