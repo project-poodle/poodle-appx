5f04ceb08e58a4870ecdb4851e3954bdf94b1817
Draco Tadpole <37931043+DracoTadpole@users.noreply.github.com>
2021-02-08 06:06:06 -0800

    appx/input/submit

---
+++ ui/app-x/spec/appx_input_submit.js
@@ -0,0 +1,87 @@
+import {
+  REGEX_VAR,
+  classes
+} from 'app-x/spec/classes.js'
+
+// type: appx/input/submit                           (~jsx|~expression)
+// name:                     # name of input         (:string)
+export const appx_input_submit = {
+
+  type: 'appx/input/submit',
+  desc: 'Submit',
+  template: {
+    kind: 'react/element',
+  },
+  _expand: true,
+  children: [
+    {
+      name: 'name',
+      desc: 'Name',
+      required: true,
+      types: [
+        {
+          kind: 'class',
+          data: 'string'
+        },
+      ],
+      _thisNode: {
+        types: 'inherit',
+        input: {
+          kind: 'input/text',
+          options: 'validation.valid_import_names()',
+          optionSelfImportNames: true,
+          optionsOnly: true,
+        },
+      },
+    },
+    {
+      name: 'BoxProps',
+      desc: 'Box Properties',
+      types: [
+        {
+          kind: 'class',
+          data: 'object'
+        }
+      ],
+      _childNode: {
+        types: 'inherit',
+        input: {
+          kind: 'input/properties',
+        },
+      },
+    },
+    {
+      name: 'style',
+      desc: 'Style',
+      types: [
+        {
+          kind: 'class',
+          data: 'object'
+        },
+      ],
+      _childNode: {
+        types: 'inherit',
+        input: {
+          kind: 'input/properties',
+          options: 'validation.valid_css_properties()',
+        },
+      },
+    },
+    {
+      name: 'children',
+      desc: 'Child Elements',
+      array: true,
+      types: [
+        {
+          kind: 'class',
+          data: 'jsx'
+        },
+      ],
+      _childNode: {
+        types: 'inherit',
+      },
+    },
+  ]
+}
+
+export default appx_input_submit
---
+++ ui/app-x/widget/InputSubmit.js
@@ -0,0 +1,71 @@
+import React, { useState, useContext, useEffect } from "react"
+import PropTypes from 'prop-types'
+import {
+  Box,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  IconButton,
+  Submit,
+  makeStyles,
+  useTheme,
+} from '@material-ui/core'
+import {
+  AddCircleOutline,
+  RemoveCircleOutline,
+} from '@material-ui/icons'
+import {
+  Controller,
+  useFormContext,
+} from 'react-hook-form'
+import {
+  AutoComplete,
+} from 'antd'
+import _ from 'lodash'
+import InputProvider from 'app-x/widget/InputProvider'
+
+const InputSubmit = (props) => {
+  // theme
+  const theme = useTheme()
+  // useFormContext
+  const {
+    register,
+    unregister,
+    errors,
+    watch,
+    handleSubmit,
+    reset,
+    setError,
+    clearErrors,
+    setValue,
+    getValues,
+    trigger,
+    control,
+    formState,
+  } = useFormContext()
+
+  // basename and propsId
+  const context = useContext(InputProvider.Context)
+
+  // return
+  return (
+    <Box
+      {...(props.BoxProps || {})}
+      style={props.style}
+      onClick={context?.onSubmit || (() => {})}
+      >
+      {
+        props.children
+      }
+    </Box>
+  )
+}
+
+InputSubmit.propTypes = {
+  BoxProps: PropTypes.object,
+  style: PropTypes.object,
+}
+
+InputSubmit.appxType = 'appx/input/submit'
+
+export default InputSubmit
